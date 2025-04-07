b29a2a2ab0dff0d3dfe44b528d06289df563cb3a
Pablowsky <pgarper032@g.educaand.es>
2025-04-07 13:42:15 +0200

    Resuelta funcio LeerPartidos

--- ProyectoED.py
+++ ProyectoED.py
@@ -2,36 +2,26 @@
 
 import csv
 
-liga = {}
-MatchList=[]
 
 def LeerPartidos():
-<<<<<<< HEAD
-   with open("liga.csv") as file:
-=======
-
+    MatchList=[]
+    liga={}
     with open("liga.csv") as file:
->>>>>>> 4734e618ba86666ba3fb397f0f46a98ee9c3e32d
+
         datos = csv.reader(file)
         next(datos)
 
         for data in datos:
             MatchList.append(data)
             liga=MatchList
+        
+    return liga
+#def impClasificacion(lista):
 
-def impClasificacion(lista):
+equipos = LeerPartidos()
 
-    for i in lista:
-        liga=lista
 
-    print(liga)
-
-LeerPartidos()
-impClasificacion(MatchList)         
-<<<<<<< HEAD
-equipos(liga)
-=======
-
+print(equipos)
 
 # def Equipos(datosliga):
 
@@ -42,4 +32,3 @@ equipos(liga)
 # def Puntos(info):
 
 # def Clasificacion(datos):
->>>>>>> 4a80979fda5626772ada90fdb2bc1df7e9721ec7
